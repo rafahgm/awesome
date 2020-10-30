109e2a59385222fa341aba7e79e3724c1dc1e9da
Rafael Morais <rafahgmorais@gmail.com>
2020-10-30 00:34:33 -0300

    new theme

--- rc.lua
+++ rc.lua
@@ -23,20 +23,17 @@ naughty.connect_signal("request::display_error", function(message, startup)
 end)
 -- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
+-- {{{ Themes
+local themes = {"default", "gtk", "sky", "xresources", "zenburn", "transparent"}
+local theme_name = themes[6]
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/" .. theme_name .. "/theme.lua")
+-- }}}
+
+-- {{{ Variable definitions
 terminal = "kitty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 -- }}}
 
