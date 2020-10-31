0b3d18df86c4212c086731d0281667a45547e96e
Rafael Morais <rafahgmorais@gmail.com>
2020-10-31 12:56:56 -0300

    removed scroll in the desktop

--- keys.lua
+++ keys.lua
@@ -11,11 +11,11 @@ editor = os.getenv("EDITOR") or "nano";
 editor_cmd = terminal .. " -e " .. editor;
 
 -- {{{ Mouse bindings
-awful.mouse.append_global_mousebindings({
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewprev),
-    awful.button({ }, 5, awful.tag.viewnext),
-})
+-- awful.mouse.append_global_mousebindings({
+--     awful.button({ }, 3, function () mymainmenu:toggle() end),
+--     awful.button({ }, 4, awful.tag.viewprev),
+--     awful.button({ }, 5, awful.tag.viewnext),
+-- })
 -- }}}
 
 -- {{{ Key bindings
